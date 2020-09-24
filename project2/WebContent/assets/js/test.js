97e282df952c9fed0bb476f119920c07644f413f
SMT076 <SMT076@59.3.254.202>
2020-09-24 20:13:26 +0900

    RSES test 추가

---
+++ project2/WebContent/assets/js/test.js
@@ -0,0 +1,11 @@
+'user strict'
+
+$(function () {
+ 
+	 //radio value값 가져오기(getter)
+ 	$("#get_btn").click(function () {
+  	var radioVal=$("input[name='test_Rses']:checked").val();
+ 	 //alert("radioVal : " +radioVal);
+	
+	});
+});
--- project2/src/kr/mmem/controller/testController.java
+++ project2/src/kr/mmem/controller/testController.java
@@ -1,6 +1,8 @@
 package kr.mmem.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +14,11 @@ public class testController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		
-		
-		
+			PrintWriter out = response.getWriter();
+			
+			String number= request.getParameter("testScore");
+			out.print(number);
+			
 		
 		
 	}
