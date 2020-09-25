7cbbaa747e26dad1e2ece86adad448f0ca39ddda
SMT076 <SMT076@59.3.254.202>
2020-09-25 09:16:12 +0900

    중복mmemberController package 제거 202009250916

--- project2/src/kr/mmem/controller/testController.java
+++ project2/src/kr/mmem/controller/testController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package kr.mmem.controller;
 
 import java.io.IOException;
@@ -65,34 +65,11 @@ public class testController extends HttpServlet {
 			}else {
 				out.print("½ÇÆÐ");
 			}
-	}
 
-}
-=======
-package kr.mmem.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/test")
-public class testController extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-			PrintWriter out = response.getWriter();
-			
-			String number= request.getParameter("testScore");
-			out.print(number);
 			
 		
 		
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/MahangProject/SMHRD_Project2.git
